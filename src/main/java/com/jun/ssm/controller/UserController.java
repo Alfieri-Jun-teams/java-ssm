e98c61d8dc72d7b6234278fcfc9cb13b66da23e4
Wang Jun <wangjun@istartech.cn>
2018-10-04 19:24:41 +0800

    解决中文乱码问题

--- src/main/java/com/jun/ssm/controller/UserController.java
+++ src/main/java/com/jun/ssm/controller/UserController.java
@@ -13,6 +13,7 @@ package com.jun.ssm.controller;
 
 import com.jun.ssm.model.User;
 import com.jun.ssm.service.IUserService;
+import com.sun.mail.iap.Response;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,21 +22,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 
 @Controller()
-@RequestMapping("api/user")
+@RequestMapping(value = "api/user")
 public class UserController {
     private Logger logger = Logger.getLogger(UserController.class);
 
     @Resource
     private IUserService userService;
 
-    @RequestMapping("/showUser")
-    public @ResponseBody User showUser(){
+    @RequestMapping(value = "showUser",produces = "application/json;charset=UTF-8")
+    public @ResponseBody List<User> showUser(HttpServletRequest request,
+                                             HttpServletResponse response){
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("UTF-8");
         logger.info("查询所有用户信息");
         List<User> userList = userService.getAllUser();
-        return userList.get(0);
+        return userList;
     }
 }
